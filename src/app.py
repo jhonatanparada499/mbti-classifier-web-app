247543a5415e2c31244b85e9aea0edbbd434cbde
Jhonatan Parada <jhonatanparada499@gmail.com>
2026-04-01 16:10:25 -0400

    feat: add starter backend logic

---
+++ src/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def main_page():
+    if request.method == "POST":
+        text = request.form.get("user_text")
+        return redirect(url_for("prediction", text=text))
+    return render_template("index.html")
+
+
+@app.route("/prediction")
+def prediction():
+    text = request.args.get("text")
+    predictions = {
+        "class1": text,
+        "class2": text,
+    }
+
+    return render_template("predict.html", predictions=predictions)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
